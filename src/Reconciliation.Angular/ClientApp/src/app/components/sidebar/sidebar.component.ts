55bbbb95b7836f517e471fdc353a2c0fa6a79c2a
Md. Sabbir Ahamed <sabbiryan@gmail.com>
2020-05-24 22:26:06 +0600

    command run on client "npx tslint-to-eslint-config"

--- src/Reconciliation.Angular/ClientApp/src/app/components/sidebar/sidebar.component.ts
+++ src/Reconciliation.Angular/ClientApp/src/app/components/sidebar/sidebar.component.ts
@@ -13,6 +13,7 @@ export const ROUTES: RouteInfo[] = [
     { path: '/requisitions', title: 'Requisitions',  icon: 'ni-basket text-primary', class: '' },
     { path: '/expenses', title: 'Expenses',  icon: 'ni-bag-17 text-primary', class: '' },
     { path: '/reconciliations', title: 'Reconciliations',  icon: 'ni-ambulance text-primary', class: '' },
+    { path: '/project', title: 'Projects',  icon: 'ni-ambulance text-primary', class: '' },
     { path: '/icons', title: 'Icons',  icon:'ni-planet text-blue', class: '' },
     // { path: '/maps', title: 'Maps',  icon:'ni-pin-3 text-orange', class: '' },
     // { path: '/user-profile', title: 'User profile',  icon:'ni-single-02 text-yellow', class: '' },
